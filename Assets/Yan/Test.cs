5b66abe06d50bbf1de4d2fcd232e08470391b610
闫辰祥 <1796655849@qq.com>
2023-02-22 15:14:52 +0800

    分支的提交测试

---
+++ Assets/Yan/Test.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 作者：闫辰祥
+/// </summary>
+public class Test : MonoBehaviour
+{
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
