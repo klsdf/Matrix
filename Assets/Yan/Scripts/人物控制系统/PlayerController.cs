aff89898e36b3af5ab50cb0ac2797be9db9a1b9f
闫辰祥 <1796655849@qq.com>
2023-02-22 16:59:41 +0800

    增加了人物控制

---
+++ Assets/Yan/Scripts/人物控制系统/PlayerController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 作者：闫辰祥
+/// </summary>
+public class PlayerController : MonoBehaviour
+{
+    private float speed = 3.0f;
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        var x = Input.GetAxis("Horizontal");
+        var y = Input.GetAxis("Vertical");
+
+
+        transform.position += speed * Time.deltaTime * new Vector3(x, y,0);
+    }
+}
